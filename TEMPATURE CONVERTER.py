ca5bf125880ad30c8699822c11a3b7310a53848b
orrrka <63540405+orrrka@users.noreply.github.com>
2020-06-17 11:24:02 +0300

    Update TEMPATURE CONVERTER.py

--- TEMPATURE CONVERTER.py
+++ TEMPATURE CONVERTER.py
@@ -1,30 +1,34 @@
-temperature = str(input("Hello! \nInsert the temperature you would like to convert:"))
+temp = str(input("Hello! \nInsert the temperature you would like to convert:"))
 
-def conversion(temperature):
-    if len(temperature)<2:
+def conversion(temp):
+    if len(temp)<2:
         return "Temperature is invalid, please add c or f."
-    elif "C" in temperature:
-        num=temperature.replace("C" , "")
+    elif "C" in temp:
+        num=temp.replace("C" , "")
         number = float(num)
         conversion = str((number*9+160)/5)
         return conversion+"F"
-    elif "c" in temperature:
-        num=temperature.replace("c" , "")
+    elif "c" in temp:
+        num=temp.replace("c" , "")
         number = float(num)
         conversion = str((number*9+160)/5)
         return conversion+"f"    
-    elif "F" in temperature:
-        num=temperature.replace("F" , "")
+    elif "F" in temp:
+        num=temp.replace("F" , "")
         number = float(num)
         conversion = str((number*5-160)/9)
         return conversion+"C"
-    elif "f" in temperature:
-        num=temperature.replace("f" , "")
+    elif "f" in temp:
+        num=temp.replace("f" , "")
         number = float(num)
         conversion = str((number*5-160)/9)
         return conversion+"c"
     else:
         return "Temperature is invalid, please try again."
+
+
+print(conversion(temp))
+
 
 
 print(conversion(temperature))
